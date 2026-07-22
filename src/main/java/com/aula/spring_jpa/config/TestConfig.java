2a4fe3163fdd30048109052f2b2ef8a8e221105c
Anderson Barg <andbarg@yahoo.com.br>
2026-07-22 18:33:41 -0300

    Category

--- src/main/java/com/aula/spring_jpa/config/TestConfig.java
+++ src/main/java/com/aula/spring_jpa/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.aula.spring_jpa.entities.Category;
 import com.aula.spring_jpa.entities.Order;
 import com.aula.spring_jpa.entities.User;
 import com.aula.spring_jpa.entities.enums.OrderStatus;
+import com.aula.spring_jpa.repositories.CategoryRepository;
 import com.aula.spring_jpa.repositories.OrderRepository;
 import com.aula.spring_jpa.repositories.UserRepository;
 
@@ -23,9 +25,19 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "999888777", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "999777555", "123456");
@@ -37,6 +49,7 @@ public class TestConfig implements CommandLineRunner {
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
 	}
 	
 	
