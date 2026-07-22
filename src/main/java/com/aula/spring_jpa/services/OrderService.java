4d2297a8b800c032b09604e627bd9e2ca0f2f5ff
Anderson Barg <andbarg@yahoo.com.br>
2026-07-22 12:38:51 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/aula/spring_jpa/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.aula.spring_jpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aula.spring_jpa.entities.Order;
+import com.aula.spring_jpa.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
